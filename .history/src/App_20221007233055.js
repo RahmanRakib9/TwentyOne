b75330ed15a790ac53177fbd2bbdb734898aad57
RahmanRakib9 <rahmanrakibur6589@gmail.com>
2022-10-07 23:43:59 +0600

    project initialize

--- .history/src/App_20221007233055.js
+++ .history/src/App_20221007233055.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div class="radial-progress" style="--value:70; --size:12rem; --thickness: 2rem;">70%</div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
